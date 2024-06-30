f3dfa2015b8b3b76b5117c032008c8f8497ee950
50Fifty <jenseanfoo@gmail.com>
2024-06-30 11:57:29 +0800

    Init

---
+++ README.md
@@ -0,0 +1,30 @@
+# auth-scheduled-backup-import
+
+This script is to backup the Firebase users that were exported by the [Backup Users to Cloud Storage extension](https://extensions.dev/extensions/50fifty/auth-scheduled-backup)
+
+## 1. Pre-requisites
+
+> [!IMPORTANT] 
+> This script is requires the following pre-requisites to be set up before running the script.
+
+### Edit the `index.ts` file
+Edit the `hash` parameter in the `importUsers` method in the `index.ts` file to match the key and salt separator that was used in your Firebase project.
+
+If you were using the default Firebase configuration, just fill in the signer key and salt separator. If you used a custom configuration, see this [Firebase documentation](https://firebase.google.com/docs/auth/admin/import-users) for more information.
+
+Use the signer key and salt separator from the Firebase Console under Firebase Authentication -> Users -> Click on the three dots -> password hash parameters
+
+### Download the service account key from your Firebase project
+1. Go to the Firebase Console
+2. Click on the gear icon next to Project Overview
+3. Click on Project Settings
+4. Click on the Service Accounts tab
+5. Click on the Generate new private key button (Node.js is the default option, so you can just click Generate Key)
+6. Save the file to the root of this repository and name it `service-account-key.json`
+
+## 2. Running the script
+1. Clone this repository
+2. Run `npm install`
+3. Edit the `index.ts` file as described in the pre-requisites
+4. Download and place the service account key in the root of this project as described in the pre-requisites
+5. Run `npm run start`
---
+++ src/index.ts
@@ -0,0 +1,74 @@
+import * as admin from 'firebase-admin';
+import { UserImportRecord } from 'firebase-admin/lib/auth/user-import-builder';
+import * as fs from 'fs';
+import * as path from 'path';
+
+const serviceAccountPath = path.join(__dirname, '../service-account-key.json');
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccountPath),
+});
+
+const dataDirectory = path.join(__dirname, '../data');
+
+const chunkArray = <T>(array: T[], size: number): T[][] => {
+  const result: T[][] = [];
+  for (let i = 0; i < array.length; i += size) {
+    result.push(array.slice(i, i + size));
+  }
+  return result;
+};
+
+const importUserChunk = async (chunk: UserImportRecord[]) => {
+  try {
+    const results = await admin.auth().importUsers(
+      chunk,
+      {
+        hash: { 
+          // IMPORTANT: Use the same password hash parameters. Refer to configuration in Firebase Console. 
+          // If you were using the default Firebase configuration, just fill in the signer key and salt separator.
+          // Use the signer key and salt separator from the Firebase Console under Firebase Authentication -> Users -> Click on the three dots -> password hash parameters
+          algorithm: 'SCRYPT',
+          key: Buffer.from('<base64-signer-key>', 'base64'),
+          saltSeparator: Buffer.from('base64-salt-separator', 'base64'),
+          rounds: 8,
+          memoryCost: 14,
+        },
+      },
+    );
+
+    results.errors.forEach((indexedError) => {
+      console.log(`Error importing user ${indexedError.index}:`, indexedError.error);
+    });
+  } catch (error) {
+    console.log('Error importing users:', error);
+  }
+};
+
+const importUsersFromFile = async (filePath: string) => {
+  const fileContent = fs.readFileSync(filePath, 'utf-8');
+  const users: UserImportRecord[] = JSON.parse(fileContent).map((user: any) => ({
+    ...user,
+    passwordHash: Buffer.from(user.passwordHash, 'base64'),
+    passwordSalt: Buffer.from(user.passwordSalt, 'base64'),
+  }));
+
+  const userChunks = chunkArray(users, 1000);
+  for (const chunk of userChunks) {
+    await importUserChunk(chunk);
+  }
+};
+
+const importAllUsers = async () => {
+  const files = fs.readdirSync(dataDirectory).filter((file) => file.endsWith('.json'));
+  for (const file of files) {
+    const filePath = path.join(dataDirectory, file);
+    await importUsersFromFile(filePath);
+  }
+};
+
+importAllUsers().then(() => {
+  console.log('All users imported successfully');
+}).catch((error) => {
+  console.error('Error importing users:', error);
+});
